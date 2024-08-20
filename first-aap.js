6292b727e46b7a4e95840635b7a19292e35ad498
hbehera27 <harishb2727@gmail.com>
2024-08-20 20:51:10 +0530

    create server

--- first-aap.js
+++ first-aap.js
@@ -1,1 +1,33 @@
-console.log('Hello World');
+//console.log('Hello World');
+
+// const fruits = ['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon'];
+// console.log(fruits.map(array => array === ' ' ? 'empty string' :array));
+
+console.log('a');
+console.log('b');
+
+const fetchDataOfC = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('c');
+        }, 3000);
+    });
+};
+
+const fetchDataOfD = () => {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve('d');
+        }, 0);
+    });
+};
+
+fetchDataOfC()
+    .then((resultC) => {
+        console.log(resultC);
+        return fetchDataOfD();
+    })
+    .then((resultD) => {
+        console.log(resultD); 
+        console.log('e');
+    });
