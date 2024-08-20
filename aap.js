1628b2746e817a3ca71d960fadcb140d94813a97
hbehera27 <harishb2727@gmail.com>
2024-08-20 23:02:44 +0530

    update with response and request

--- aap.js
+++ aap.js
@@ -1,7 +1,21 @@
+// const http = require('http');
+
+// const server = http.createServer((req,res)=>{
+//     console.log('Harish');
+// });
+
+// server.listen(4000);
+
 const http = require('http');
 
-const server = http.createServer((req,res)=>{
-    console.log('Harish');
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.headers,req.method);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
-server.listen(4000);
+server.listen(3000);
