0369a2c9c29f6260382d30f3a75c965179b43995
hbehera27 <harishb2727@gmail.com>
2024-08-21 11:46:38 +0530

    demo project

---
+++ demo.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const fs = require('fs');
+let messages = [];
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    const method = req.method;
+
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', chunk => {
+            console.log(chunk);
+            body.push(chunk); 
+        });
+
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+
+            if (message) {
+                messages.unshift(message);
+            }
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
+        });
+    }
+    if (url === '/' && method === 'GET') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body>');
+        res.write(`<p>${messages.join('<br>')}</p>`);
+        res.write('<form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form>');
+        res.write('</body>');
+        res.write('</html>');
+        return res.end(); 
+    }
+});
+
+server.listen(3000);
